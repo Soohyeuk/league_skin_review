1a6e5ba3948523d3c70084340217b306a6a6cabb
Soohyeuk <csh040816@gmail.com>
2024-09-06 23:55:45 -0400

    getting rid of commits with sensitive information

---
+++ backend/config.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'uVFEBycPDPTrBUhdGIZzaqfKtjMazczr',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '55544',
+    }
+}
+SECRET_KEY = 'django-insecure-45tpm0$0q3)g733sv88!no8rgn-=#q(na5(dc-!u1u$v@mcy3g'
